c50d8781a0936383785d3347b814922ba3e9976f
kinasch <janik@schoenberg.name>
2021-05-26 23:03:44 +0200

    second type of obstacle

--- Assets/Scripts/obstacleBeahviour.cs
+++ Assets/Scripts/obstacleBeahviour.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 public class obstacleBeahviour : MonoBehaviour
 {
     public float rotationValue = 2;
+    public float pushSpeed = 2;
+
+    private float max,min;
+
+    private bool isRunningPusher = false;
+
+    private void Start()
+    {
+        max = 8;
+        min = 0;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -15,6 +26,28 @@ public class obstacleBeahviour : MonoBehaviour
             case "rotatingBlock":
                 transform.Rotate(new Vector3(0, rotationValue, 0));
                 break;
+            case "pusher":
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, Mathf.PingPong(Time.time*10,max-min)+min);
+                break;
         }
+    }
+
+    IEnumerator PushSequence()
+    {
+        isRunningPusher = true;
+        while (transform.localPosition.z < 7.5f)
+        {
+            var transform1 = transform;
+            transform1.position += transform1.forward * (Time.deltaTime);
+        }
+        yield return new WaitForSeconds(2);
+        
+        while (transform.localPosition.z > 0.2f)
+        {
+            var transform1 = transform;
+            transform1.position -= transform1.forward * (Time.deltaTime);
+        }
+        yield return new WaitForSeconds(2);
+        isRunningPusher = false;
     }
 }
